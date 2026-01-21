0eee4bc1949dfacdcef1493a5cf2e28c323926d4
Rockylane01 <rockylane01@gmail.com>
2026-01-20 19:44:14 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission3Assignment
+{
+    internal class FoodItem
+    {
+        string name = "";
+        string category = "";
+        int quantity = 0;
+        DateTime expirationDate = DateTime.Now;
+
+        public FoodItem(string name, string category, int quantity, DateTime exp) 
+        {
+            this.name = name;
+            this.category = category;
+            this.quantity = quantity;
+            this.expirationDate = exp;
+        }
+
+        // Print List of Current Food Items
+
+    }
+}
